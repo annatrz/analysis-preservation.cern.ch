3ac453519b84a39d3c14c3b2570b8f9e67ec6543
Anna Trzcinska <anna.trzcinska@cern.ch>
2019-08-06 15:28:26 +0200

    schemas: add schemas fixtures
    Signed-off-by: Anna Trzcinska <anna.trzcinska@cern.ch>

--- tests/unit/schemas/test_utils.py
+++ tests/unit/schemas/test_utils.py
@@ -1,58 +1,57 @@
 from pytest import mark
 
 from cap.modules.schemas.models import Schema
-from cap.modules.schemas.utils import add_or_update_schema
+from cap.modules.schemas.utils import add_schema_from_fixture
 
 
-@mark.skip('method add_or_update_schema has to be updated')
 def test_add_or_update_schema_when_schema_does_not_exist_create_new_one(db):  # noqa
-    data = {
-        'experiment': 'CMS',
-        'fullname': 'Test Schema',
-        'deposit_schema': {'field': 'string'}
-    }
+    data = dict(name='new-schema',
+                version='1.2.3',
+                fullname='New fullname',
+                deposit_schema={'title': 'deposit_schema'},
+                deposit_options={'title': 'deposit_options'},
+                record_schema={'title': 'record_schema'},
+                record_options={'title': 'record_options'},
+                record_mapping={'doc': {'properties': {
+                    "title": {"type": "text"}}}},
+                deposit_mapping={'doc': {'properties': {
+                    "keyword": {"type": "keyword"}}}},
+                is_indexed=True,
+                use_deposit_as_record=True
+                )
 
-    add_or_update_schema(fullpath='records/ana1-v1.0.2.json',
-                         data=data)
+    add_schema_from_fixture(data=data)
 
-    schema = Schema.get_by_fullpath('records/ana1-v1.0.2.json')
-    assert schema.name == 'records/ana1'
-    assert schema.fullname == data['fullname']
-    assert schema.experiment == data['experiment']
-    assert schema.deposit_schema == data['jsonschema']
-    assert schema.major == 1
-    assert schema.minor == 0
-    assert schema.patch == 2
+    schema = Schema.get('new-schema', version='1.2.3')
+    for key, value in data.items():
+        assert getattr(schema, key) == value
 
 
-@mark.skip('method add_or_update_schema has to be updated')
 def test_add_or_update_schema_when_schema_already_exist_updates_json_for_schema(db):  # noqa
+    updated_data = dict(name='new-schema',
+                        version='1.1.1',
+                        experiment='LHCb',
+                        fullname='New fullname',
+                        deposit_schema={'title': 'deposit_schema'},
+                        deposit_options={'title': 'deposit_options'},
+                        record_schema={'title': 'record_schema'},
+                        record_options={'title': 'record_options'},
+                        record_mapping={'doc': {'properties': {
+                            "title": {"type": "text"}}}},
+                        deposit_mapping={'doc': {'properties': {
+                            "keyword": {"type": "keyword"}}}},
+                        is_indexed=True,
+                        use_deposit_as_record=True
+                        )
     db.session.add(Schema(**{
-        'name': 'records/ana1',
-        'major': 1,
-        'minor': 0,
-        'patch': 2,
+        'name': 'new-schema',
         'experiment': 'CMS',
         'fullname': 'Old Schema',
-        'deposit_schema': {'field': 'initial'}
     }))
     db.session.commit()
-    updated_data = {
-        'experiment': 'Updated',
-        'fullname': 'Updated Schema',
-        'deposit_schema': {
-            'updated': 'string'
-        }
-    }
 
-    add_or_update_schema(fullpath='records/ana1-v1.0.2.json',
-                         data=updated_data)
+    add_schema_from_fixture(data=updated_data)
 
-    schema = Schema.get_by_fullpath('records/ana1-v1.0.2.json')
-    assert schema.name == 'records/ana1'
-    assert schema.fullname == updated_data['fullname']
-    assert schema.experiment == updated_data['experiment']
-    assert schema.deposit_schema == updated_data['deposit_schema']
-    assert schema.major == 1
-    assert schema.minor == 0
-    assert schema.patch == 2
+    schema = Schema.get('new-schema', version='1.1.1')
+    for key, value in updated_data.items():
+        assert getattr(schema, key) == value
